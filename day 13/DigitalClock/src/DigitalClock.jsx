750cdfb4bd014b0f2a87385d666d7ea2e88ea236
anasnadkar45 <anasnadkar23@gmail.com>
2024-08-05 12:10:37 +0530

    Day 13 Digital Clock

---
+++ day 13/DigitalClock/src/DigitalClock.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+const DigitalClock = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+    useEffect(()=>{
+        const updateTime = () =>{
+            const now = new Date();
+            setSeconds(now.getSeconds());
+            setMinutes(now.getMinutes());
+            setHours(now.getHours());
+        }
+
+        updateTime();
+
+        const intervelId = setInterval(updateTime, 1000);
+
+        return () => clearInterval(intervelId);
+    })
+
+    const formatTime = (value) =>{
+        return value<10 ? `0${value}` : value
+    }
+
+    return (
+        <div className='w-[500px] mx-auto border border-gray-100 p-5'>
+            <h1>Digital Clock</h1>
+            <div className='flex items-center justify-center pt-4 gap-x-4 text-3xl'>
+                <h2>{formatTime(hours)}</h2> : <h2>{formatTime(minutes)}</h2> : <h2>{formatTime(seconds)}</h2>
+            </div>
+        </div>
+    )
+}
+
+export default DigitalClock
