166f7c7b245cdbaf4177b8facfa01b18898d74d1
anasnadkar45 <anasnadkar23@gmail.com>
2024-08-31 09:07:35 +0530

    Redux practice

--- redux-practice/src/App.jsx
+++ redux-practice/src/App.jsx
@@ -1,18 +1,46 @@
 import './App.css'
 import { useFetch } from './CustomHooks/useFetch';
+import { useSelector, useDispatch } from 'react-redux'
+import { addToCart, removeFromCart } from './features/cart/cartSlice';
+import { useState } from 'react';
 
 function App() {
   const { data, loading } = useFetch('https://fakestoreapi.com/products');
+  const [showCart, setShowCart] = useState(false)
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart.cart);
 
   return (
-    <div style={{display:'grid', gridTemplateColumns:'1fr 1fr 1fr', gap:'10px'}}>
-      {!loading ? (data.map((item) => (
-        <div key={item.id} style={{border:'1px solid', }}>
-          <img src={item.image} alt={item.title} style={{width:'300px', height:'350px'}}/>
-          <h3>{item.title}</h3>
+    <div>
+      <h2>Cart Items: {cart.length}</h2>
+      <button onClick={()=>setShowCart(!showCart)}>
+        {!showCart ? 'Show cart' : 'Home'}
+      </button>
+      {!showCart ? (
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '10px' }}>
+          {!loading ? (data.map((item) => (
+            <div key={item.id} style={{ border: '1px solid', padding: '5px' }}>
+              <img src={item.image} alt={item.title} style={{ width: '300px', height: '350px' }} />
+              <h3>{item.title}</h3>
+              <button onClick={() => dispatch(addToCart(item))}>Add To Cart</button>
+            </div>
+          ))) : (
+            <h1>Loading...</h1>
+          )}
         </div>
-      ))) : (
-        <h1>Loading...</h1>
+      ) : (
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '10px' }}>
+          {!loading ? (cart.map((item) => (
+            <div key={item.item.id} style={{ border: '1px solid', padding: '5px' }}>
+              <img src={item.item.image} alt={item.item.title} style={{ width: '300px', height: '350px' }} />
+              <h3>{item.item.title}</h3>
+              <p>count : {item.count}</p>
+              <button onClick={() => dispatch(removeFromCart(item))}>Remove from Cart</button>
+            </div>
+          ))) : (
+            <h1>Loading...</h1>
+          )}
+        </div>
       )}
     </div>
   )
--- redux-practice/src/features/cart/cartSlice.js
+++ redux-practice/src/features/cart/cartSlice.js
@@ -1,18 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    cart:[],
+    cart: [],
 }
 
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers:{
-        addToCart:(state, action)=>{
-            const item = {
-                
+    reducers: {
+        addToCart: (state, action) => {
+            const existingItem = state.cart.find(item => item.item.id === action.payload.id)
+
+            if(existingItem){
+                existingItem.count += 1;
+            }else{
+                const newItem = {
+                    count:1,
+                    item: action.payload
+                }
+                state.cart.push(newItem)
             }
         },
-        removeFromCart:()=>{},
+        removeFromCart: (state,action) => {
+            state.cart = state.cart.filter(item => item.item.id !== action.payload.item.id)
+        },
     }
 })
+
+export const { addToCart,removeFromCart } = cartSlice.actions
+
+export default cartSlice.reducer
