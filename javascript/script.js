0c6ebf12635398397389c170083103f9050f1002
anasnadkar45 <anasnadkar23@gmail.com>
2024-12-16 11:31:47 +0530

    updating codebase

--- javascript/script.js
+++ javascript/script.js
@@ -202,3 +202,154 @@
 
 // ----------------------------------------------------------------
 
+// function computeAmount(){
+//     let totalAmount = 0;
+//     this.lacs = function(amount){
+//         totalAmount += amount * 100000
+//         return this
+//     }
+
+//     this.crore = function(amount){
+//         totalAmount += amount * 10000000
+//         return this;
+//     }
+
+//     this.thousand = function(amount){
+//         totalAmount += amount * 1000
+//         return this;
+//     }
+
+//     this.value = function(){
+//         return totalAmount;
+//     }
+//     return this
+// }
+
+// console.log(computeAmount().lacs(1).lacs(1).crore(1).thousand(1).crore(1).value())
+
+// ----------------------------------------------------------
+
+// function Student() {
+//     this.name = "Saikrishna",
+//     this.exp = "8"
+// }
+// Student.prototype.company = "Hexagon"
+// let std1 = new Student();
+// std1.exp = "9"
+// let std2 = new Student();
+// std2.exp = "10"
+// console.log(std1);
+// console.log(std2)
+
+// ----------------------------------------------------------------
+
+// let items = [
+//     { color: 'red', type: 'tv', age: 18 },
+//     { color: 'silver', type: 'phone', age: 20 },
+//     { color: 'blue', type: 'book', age: 18 },
+// ];
+
+// const excludes = [
+//     { k: 'color', v: 'silver' },
+//     { k: 'type', v: 'tv' },
+// ];
+
+// function excludeItems(items, excludes) {
+//     excludes.forEach(pair => {
+//         items = items.filter(item => item[pair.k] !== pair.v);
+//     });
+//     return items;
+// }
+
+// console.log(excludeItems(items, excludes));
+
+//----------------------------------------------------------------------
+
+
+// function square() {
+//     const temp = [];
+//     for (let i = 0; i < this.length; i++) {
+//         let multiple = Number((this[i] * this[i]).toFixed(2));
+//         temp.push(multiple);
+//     }
+//     return temp;
+// }
+
+// Array.prototype.square = square;
+
+// const numbers = [0.1, -0.5, 1.1];
+// const sqNumbers = numbers.square();
+// console.log(sqNumbers); // Output: [0.01, 0.25, 1.21]
+
+// ----------------------------------------------------------------
+
+// function add(n = 0) {
+//     let temp = n;
+//     return {
+//         add: function add(num){
+//             temp += num;
+//             return this
+//         },
+//         sum: function sum(){
+//             return temp
+//         }
+//     }
+// }
+
+// const value = add(4).add(2).add(3).sum();
+// console.log(value); // Outputs: 9
+
+// const anotherValue = add(10).add(20).sum();
+// console.log(anotherValue); // Outputs: 30
+
+
+//----------------------------------------------------------------
+
+
+// Array.prototype.myMap = function (cbFn) {
+//     const temp = [];
+//     for (let i = 0; i < this.length; i++) {
+//         temp.push(cbFn(this[i]));
+//     }
+//     return temp;
+// }
+
+// const arr = [1, 'hello', [2, 3], { a: 1, b: 2 }];
+
+// const cbFn = (element) => {
+//     if (typeof element === 'number') {
+//         return element * 2
+//     } else if (typeof element === 'string') {
+//         return element.toUpperCase()
+//     } else if (Array.isArray(element)) {
+//         let temp = []
+//         for (let i = 0; i < element.length; i++) {
+//             temp.push(element[i])
+//         }
+//         return temp
+//     } else if (typeof element === 'object' && element !== null) {
+//         return Object.keys(element)
+//     }
+// }
+// const exp1 = arr.myMap(cbFn);
+// console.log(exp1)
+
+// ----------------------------------------------------------------
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const output = arr.reduce((acc, current) => {
+//     if(current % 2 === 0) {
+//         current = current + 2;
+//         acc += current
+//     }else{
+//         current = current - 1;
+//         acc += current
+//     }
+//     return acc;
+// },0)
+
+// console.log(output)
+
+// -----------------------------------------------------------
+
